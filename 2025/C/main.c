3afeca2c633cb1b779941eb155531bbeda4965e3
Lucas Ribeiro da Luz <lucasrluz.23@gmail.com>
2025-12-07 08:16:15 -0300

    feat: algoritmo finalizado

--- 2025/C/main.c
+++ 2025/C/main.c
@@ -1,54 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void get_input(char directions[], int numbers[]) {
+    FILE *file = fopen("input.txt", "r");
+
+    char line[6];
+    char number[4];
+
+    int i = 0;
+
+    while (fgets(line, sizeof(line), file) != NULL) {
+        directions[i] = line[0];
+
+        if (strlen(line) == 5) {
+            strncpy(number, line + 1, 3);
+            number[3] = '\0';
+        }
+
+        if (strlen(line) == 4) {
+            strncpy(number, line + 1, 2);
+            number[2] = '\0';
+        }
+
+        if (strlen(line) == 3) {
+            strncpy(number, line + 1, 1);
+            number[1] = '\0';
+        }
+
+        numbers[i] = atoi(number);
+
+        i++;
+    }
+}
 
 int main() {
-    // FILE *file = fopen("input.txt", "r");
+    char direction[5554];
+    int number[5554];
 
-    // char line[256];
-
-    // while (fgets(line, sizeof(line), file) != NULL) {
-    //     printf("%s", line);
-    // }
-
+    get_input(direction, number);
+    
     int count = 50;
     int zero_count = 0;
 
-    int inputs[10] = {68, 30, 48, 5, 60, 55, 1, 99, 14, 82};
-    char inputs_type[10] = {'L', 'L', 'R', 'L', 'R', 'L', 'L', 'L', 'R', 'L'};
+    // int inputs[10] = {68, 30, 48, 5, 60, 55, 1, 99, 14, 82};
+    // char inputs_type[10] = {'L', 'L', 'R', 'L', 'R', 'L', 'L', 'L', 'R', 'L'};
 
-    for (int i = 0; i < 10; i++) {
-        printf("\nExecução %d\n", i);
-        printf("count antes: %d\n", count);
-
-        if (inputs_type[i] == 'L') {
-            int result = count - inputs[i];
-
-            if (result == 0) {
-                count = 0;
-            } else if (result < 0) {
-                count = 100 - abs(result);
-            } else {
-                count = count - inputs[i];
+    for (int i = 0; i < 5554; i++) {
+        if (direction[i] == 'L') {
+          for (int ii = 0; ii < number[i];  ii++) {
+            if (count == 0) {
+              count = 100;
             }
-        } else {
-            int result = count + inputs[i];
 
-            if (result == 100) {
-                count = 0;
-            } else if (result > 100) {
-                count = result - 100;
-            } else {
-                count = result;
-            }
-        }
+            count -= 1;
+          }
 
-        printf("count depois: %d\n", count);
-
-        if (count == 0) {
+          if (count == 0) {
             zero_count += 1;
-        }
-    }
+          }
+        } else {
+          for (int ii = 0; ii < number[i]; ii++) {
+            count += 1;
 
+            if (count == 100) {
+              count = 0;
+            }
+          }
+
+          if (count == 0) {
+            zero_count += 1;
+          }
+        }
+      }
     printf("zero_count: %d", zero_count);
         
     return 0;
