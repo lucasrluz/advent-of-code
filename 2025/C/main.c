b6c897de403e4c6c1cb04ab887a28f3ca0e81881
Lucas Ribeiro da Luz <lucasrluz.23@gmail.com>
2025-12-07 08:27:05 -0300

    feat: refatoração

--- 2025/C/main.c
+++ 2025/C/main.c
@@ -34,46 +34,51 @@ void get_input(char directions[], int numbers[]) {
     }
 }
 
+int run_part_1(char directions[], int numbers[]) {
+    int dial = 50;
+    int password = 0;
+
+    for (int i = 0; i < 5554; i++) {
+        if (directions[i] == 'L') {
+            for (int j = 0; j < numbers[i]; j++) {
+                if (dial == 0) {
+                    dial = 100;
+                }
+
+                dial -= 1;
+            }
+
+            if (dial == 0) {
+                password += 1;
+            }
+        }
+
+        if (directions[i] == 'R') {
+            for (int j = 0; j < numbers[i]; j++) {
+                dial += 1;
+
+                if (dial == 100) {
+                    dial = 0;
+                }
+            }
+
+            if (dial == 0) {
+                password += 1;
+            }
+        }
+    }
+
+    return password;
+}
+
 int main() {
     char direction[5554];
     int number[5554];
 
     get_input(direction, number);
-    
-    int count = 50;
-    int zero_count = 0;
 
-    // int inputs[10] = {68, 30, 48, 5, 60, 55, 1, 99, 14, 82};
-    // char inputs_type[10] = {'L', 'L', 'R', 'L', 'R', 'L', 'L', 'L', 'R', 'L'};
-
-    for (int i = 0; i < 5554; i++) {
-        if (direction[i] == 'L') {
-          for (int ii = 0; ii < number[i];  ii++) {
-            if (count == 0) {
-              count = 100;
-            }
-
-            count -= 1;
-          }
-
-          if (count == 0) {
-            zero_count += 1;
-          }
-        } else {
-          for (int ii = 0; ii < number[i]; ii++) {
-            count += 1;
-
-            if (count == 100) {
-              count = 0;
-            }
-          }
-
-          if (count == 0) {
-            zero_count += 1;
-          }
-        }
-      }
-    printf("zero_count: %d", zero_count);
+    int password = run_part_1(direction, number);
+    printf("Part 1 password: %d", password);
         
     return 0;
 }
